1375bb43610e08c42ad797d00de83a3086093e56
RafSwiggers <rafswiggers@gmail.com>
2019-06-05 16:46:07 +0200

    added the table add function

--- script/script.js
+++ script/script.js
@@ -1,16 +1,29 @@
 function textBox() {
-    document.getElementById("holderBox").innerHTML = document.getElementById('greetingsBox').value
+    document.getElementById("holderBox").innerText = document.getElementById('greetingsBox').value
 }
-var pizza = document.getElementById('pizza');
-var name = document.getElementById('name');
-var size = document.getElementById('size');
+var pizza = document.getElementById('pizzaField');
+var textName = document.getElementById('nameField');
+var size = document.getElementById('sizeField');
+var remarks = document.getElementById('remarksField');
 
 var menu = document.getElementById("pizzaOrders");
 menu.addEventListener("keydown", function(e) {
+    var table = document.getElementById("pizzaOrders");
+
     var tableBody = document.getElementById('tableBody');
     if (e.keyCode === 13) { //checks whether the pressed key is "Enter"
-        if ((name.value.length > 0) && (size.value.length > 0) && (pizza.value.length > 0)) { //checks if the three required fields are filled in.
-            console.log("checker for values works")
+        if ((textName.value != '') && (size.value != '') && (pizza.value != '')) { //checks if the three required fields are filled in.
+            var row = table.insertRow(2);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            cell1.innerText = textName.value;
+            cell2.innerText = size.value;
+            cell3.innerText = pizza.value;
+            cell4.innerText = remarks.value;
+
+
         } else {
             console.log("else for no values works")
         }
